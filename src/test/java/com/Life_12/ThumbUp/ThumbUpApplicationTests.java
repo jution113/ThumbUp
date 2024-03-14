ec9049767e264d002bc9cbbe83ff607cdc4c7ecb
jution113 <jution113@gmail.com>
2024-03-14 16:46:38 +0900

    INIT: 프로젝트 생성

---
+++ src/test/java/com/Life_12/ThumbUp/ThumbUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Life_12.ThumbUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThumbUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
